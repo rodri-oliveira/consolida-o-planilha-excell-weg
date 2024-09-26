383e787f5874ed71ac43c02462424c1563d6feb2
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2024-09-26 16:51:42 -0300

    add logica dinamica para possivel crescimento da planilha

--- consolidar.py
+++ consolidar.py
@@ -6,6 +6,7 @@ def consolidar_planilhas(caminho_das_planilhas):
     global dataframe_consolidado
     lista_dfs = []
 
+    # Loop para percorrer todos os arquivos no diretório de planilhas
     for arquivo in os.listdir(caminho_das_planilhas):
         if arquivo.endswith('.xlsx'):
             caminho_completo = os.path.join(caminho_das_planilhas, arquivo)
@@ -28,11 +29,15 @@ def consolidar_planilhas(caminho_das_planilhas):
                         planned_effort = row['Planned effort']
                         estimate_effort = row.iloc[5] if len(row) > 5 else None
 
-                        colunas_meses = df.columns[8:25]
+                        # Identifica as colunas de meses dinamicamente, buscando por padrões nos nomes das colunas
+                        colunas_meses = [col for col in df.columns if any(mes in col for mes in ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])]
+
+                        ano_base = 2024
+                        meses_por_ano = 12
 
                         for idx, mes in enumerate(colunas_meses):
                             valor_hora_mes = row[mes]
-                            ano = 2024 if idx < 5 else 2025
+                            ano = ano_base + (idx // meses_por_ano)  # Ajusta o ano automaticamente
 
                             nova_linha = {
                                 'Epic': epic,
@@ -53,7 +58,7 @@ def consolidar_planilhas(caminho_das_planilhas):
         # Exclui colunas indesejadas
         dataframe_consolidado.drop(columns=['Horas disponíveis', 'Total de esforço'], inplace=True, errors='ignore')
 
-        caminho_para_salvar_arquivo = 'C:/consolida-o-planilha-excell-weg/planilhas-consolidadas/planilha_consolidada.xlsx'
+        caminho_para_salvar_arquivo = 'C:/consolidar-planilha-weg/planilhas-consolidadas/planilha_consolidada.xlsx'
         os.makedirs(os.path.dirname(caminho_para_salvar_arquivo), exist_ok=True)
         dataframe_consolidado.to_excel(caminho_para_salvar_arquivo, index=False)
 
