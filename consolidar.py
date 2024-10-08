53ebb8d7f162318d2bf873daebccfd76bcb89ae2
rodri-oliveira <rodrigoleao.ci@gmail.com>
2024-10-08 09:56:14 -0300

    corrigido formato das colunas retirando espaços e caracteres

--- consolidar.py
+++ consolidar.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import pandas as pd
+import unidecode
 from io import BytesIO
 from auth import enviar_para_sharepoint, obter_token_sharepoint, buscar_arquivos_pasta
 from dotenv import load_dotenv
@@ -10,14 +11,15 @@ token = obter_token_sharepoint()
 SITE_URL = os.getenv('SITE_URL')
 
 # Função principal para consolidar planilhas do SharePoint
-def consolidar_planilhas_sharepoint(lista_arquivos_sharepoint, token):
-    
-    """Consolida todas as planilhas dos arquivos Excel do SharePoint, ignorando a aba 'Backlog'.
+import unidecode
 
+def consolidar_planilhas_sharepoint(lista_arquivos_sharepoint, token):
+    """Consolida todas as planilhas dos arquivos Excel do SharePoint, ignorando a aba 'Backlog'.
+    
     Args:
         lista_arquivos_sharepoint (list): Lista de URLs dos arquivos no SharePoint.
         token (str): O token de acesso para autenticação.
-
+    
     Returns:
         None: A função não retorna valor, mas faz o upload do arquivo consolidado para o SharePoint.
     """
@@ -28,29 +30,31 @@ def consolidar_planilhas_sharepoint(lista_arquivos_sharepoint, token):
     for url_arquivo in lista_arquivos_sharepoint:
         print(f"Processando arquivo: {url_arquivo}")
         
-        #montar endpoint do arquivo
+        # Montar endpoint do arquivo
         END_POINT = f"https://weg365.sharepoint.com/teams/BR-TI-TIN/_api/Web/GetFileByServerRelativePath(decodedurl='{url_arquivo}')/$value"
-        # Usa requests para acessar diretamente o arquivo no SharePoint
         response = requests.get(END_POINT, headers={'Authorization': f'Bearer {token}'})
         
         if response.status_code != 200:
             print(f"Erro ao acessar arquivo: {url_arquivo} - {response.status_code}")
             continue
         
-        # Usa o conteúdo diretamente, sem baixar
-        arquivo_excel = BytesIO(response.content)  # Lê o conteúdo da resposta no formato Excel
+        # Lê o conteúdo da resposta no formato Excel
+        arquivo_excel = BytesIO(response.content)
         xls = pd.ExcelFile(arquivo_excel)
         
         for nome_aba in xls.sheet_names:
-            # Verifica se a aba é "Backlog" e ignora
+            # Ignora a aba "Backlog"
             if 'Backlog' in nome_aba:
                 print(f"Ignorando aba: '{nome_aba}'")
                 continue
 
             df = pd.read_excel(xls, sheet_name=nome_aba)
 
+            # Renomeia colunas (remove acentos e substitui espaços por underscores)
+            df.columns = [unidecode.unidecode(col).replace(" ", "_") for col in df.columns]
+
             # Verifica se a aba tem as colunas mínimas necessárias
-            if 'Planned effort' in df.columns and df.shape[1] > 5:
+            if 'Planned_effort' in df.columns and df.shape[1] > 5:
                 valor_secao = df.iloc[3, 0]  # Captura a célula A4
                 valor_equipe = df.iloc[4, 0]  # Captura a célula A5
 
@@ -60,8 +64,8 @@ def consolidar_planilhas_sharepoint(lista_arquivos_sharepoint, token):
 
                     epic = row['Epic'] if 'Epic' in df.columns else ''
                     status = row['Status'] if 'Status' in df.columns else ''
-                    due_date = row['Due Date'] if 'Due Date' in df.columns else ''
-                    planned_effort = row['Planned effort']
+                    due_date = row['Due_Date'] if 'Due_Date' in df.columns else ''
+                    planned_effort = row['Planned_effort']
                     estimate_effort = row.iloc[5] if len(row) > 5 else None
 
                     colunas_meses = df.columns[8:]
@@ -83,14 +87,14 @@ def consolidar_planilhas_sharepoint(lista_arquivos_sharepoint, token):
                             nova_linha = {
                                 'Epic': epic,
                                 'Status': status,
-                                'Due Date': due_date,
+                                'Due_Date': due_date,
                                 'Assignee': row['Assignee'] if 'Assignee' in df.columns else '',
-                                'Planned Effort': planned_effort,
-                                'Estimate Effort': estimate_effort,
-                                'MÊS': mes_abreviado,
+                                'Planned_Effort': planned_effort,
+                                'Estimate_Effort': estimate_effort,
+                                'MES': mes_abreviado,
                                 'ANO': ano,
-                                'Horas mês': valor_hora_mes,
-                                'Seção': valor_secao,
+                                'Horas_mes': valor_hora_mes,
+                                'Secao': valor_secao,
                                 'Equipe': valor_equipe
                             }
                             lista_dfs.append(nova_linha)
@@ -101,16 +105,15 @@ def consolidar_planilhas_sharepoint(lista_arquivos_sharepoint, token):
     # Consolida os dados em um DataFrame
     if lista_dfs:
         dataframe_consolidado = pd.DataFrame(lista_dfs)
-        dataframe_consolidado.drop(columns=['Horas disponíveis', 'Total de esforço'], inplace=True, errors='ignore')
+        dataframe_consolidado.drop(columns=['Horas_disponiveis', 'Total_de_esforco'], inplace=True, errors='ignore')
 
-        # Salva o DataFrame em um objeto de memória (BytesIO), sem salvar localmente
         # Salva o DataFrame em um objeto de memória (BytesIO), sem salvar localmente
         arquivo_memoria = BytesIO()
         dataframe_consolidado.to_csv(arquivo_memoria, index=False)  # Mudança aqui para salvar como CSV
-        arquivo_memoria.seek(0)  # Move o ponteiro de volta ao início do arquivo
+        arquivo_memoria.seek(0)
 
         # Nome do arquivo a ser salvo no SharePoint
-        nome_arquivo_sharepoint = "planilha_consolidada.csv"  # Mudança aqui para .csv
+        nome_arquivo_sharepoint = "planilha_consolidada.csv"
 
         try:
             # Faz o upload do arquivo diretamente da memória para o SharePoint
@@ -120,8 +123,11 @@ def consolidar_planilhas_sharepoint(lista_arquivos_sharepoint, token):
     else:
         print("Nenhuma planilha foi consolidada. Verifique os arquivos de entrada.")
 
-
 # Função principal para consolidar a aba "Backlog" e enviar para o SharePoint
+import unidecode
+
+import unidecode
+
 def consolidar_aba_backlog_sharepoint(lista_arquivos_sharepoint, token):
     """Consolida as planilhas da aba 'Backlog' de todos os arquivos Excel no SharePoint."""
 
@@ -155,8 +161,11 @@ def consolidar_aba_backlog_sharepoint(lista_arquivos_sharepoint, token):
                 # Lê a aba "Backlog" como DataFrame
                 df = pd.read_excel(xls, sheet_name=nome_aba)
 
+                # Renomeia colunas (remove acentos e substitui espaços por underscores)
+                df.columns = [unidecode.unidecode(col).replace(" ", "_") for col in df.columns]
+
                 # Verifica se as colunas necessárias existem e o DataFrame tem colunas suficientes
-                if 'Estimated effort' in df.columns and df.shape[1] > 5:
+                if 'Estimated_effort' in df.columns and df.shape[1] > 5:
                     # Obtém os valores da "Seção" e "Equipe"
                     valor_secao = df.iloc[3, 0]  # Captura a célula A4
                     valor_equipe = df.iloc[4, 0]  # Captura a célula A5
@@ -169,17 +178,17 @@ def consolidar_aba_backlog_sharepoint(lista_arquivos_sharepoint, token):
                         # Captura os dados relevantes
                         epic = row['Epic'] if 'Epic' in df.columns else ''
                         status = row['Status'] if 'Status' in df.columns else ''
-                        due_date = row['Due Date'] if 'Due Date' in df.columns else ''
-                        estimated_effort = row['Estimated effort']  # Valor da coluna Estimated effort
+                        due_date = row['Due_Date'] if 'Due_Date' in df.columns else ''
+                        estimated_effort = row['Estimated_effort']  # Valor da coluna Estimated effort
 
                         # Cria a nova linha para o DataFrame consolidado
                         nova_linha = {
                             'Epic': epic,
                             'Status': status,
-                            'Due Date': due_date,
+                            'Due_Date': due_date,
                             'Assignee': row['Assignee'] if 'Assignee' in df.columns else '',
-                            'Estimated Effort': estimated_effort,
-                            'Seção': valor_secao,    # Adiciona o valor da Seção
+                            'Estimated_Effort': estimated_effort,
+                            'Secao': valor_secao,    # Adiciona o valor da Seção
                             'Equipe': valor_equipe   # Adiciona o valor da Equipe
                         }
                         lista_dfs.append(nova_linha)
@@ -189,7 +198,7 @@ def consolidar_aba_backlog_sharepoint(lista_arquivos_sharepoint, token):
         dataframe_consolidado = pd.DataFrame(lista_dfs)
 
         # Exclui colunas indesejadas
-        dataframe_consolidado.drop(columns=['Horas disponíveis', 'Total de esforço'], inplace=True, errors='ignore')
+        dataframe_consolidado.drop(columns=['Horas_disponíveis', 'Total_de_esforço'], inplace=True, errors='ignore')
 
         # Exclui linhas onde a primeira coluna está em branco
         dataframe_consolidado = dataframe_consolidado[dataframe_consolidado.iloc[:, 0].notna()]
@@ -198,7 +207,6 @@ def consolidar_aba_backlog_sharepoint(lista_arquivos_sharepoint, token):
         url_diretorio_sharepoint = "https://weg365.sharepoint.com/sites/BR-TI-TIN/DEV_AlocacaoRecursos/Consolidado"
         nome_arquivo_sharepoint = "backlog_consolidado.xlsx"
         
-        # Salva o DataFrame em um objeto de memória (BytesIO), sem salvar localmente
         # Salva o DataFrame em um objeto de memória (BytesIO), sem salvar localmente
         arquivo_memoria = BytesIO()
         dataframe_consolidado.to_csv(arquivo_memoria, index=False)  # Mudança aqui para salvar como CSV
@@ -215,6 +223,7 @@ def consolidar_aba_backlog_sharepoint(lista_arquivos_sharepoint, token):
 
     else:
         print("Nenhuma planilha foi consolidada. Verifique os arquivos de entrada.")
+
 def consolidar_horas_backlog_sharepoint(lista_arquivos_sharepoint, token):
     """Consolida os dados das abas 'Backlog' de todos os arquivos Excel no SharePoint."""
     lista_dfs = []
@@ -244,25 +253,28 @@ def consolidar_horas_backlog_sharepoint(lista_arquivos_sharepoint, token):
                 print(f"Processando aba: '{nome_aba}'")
                 df = pd.read_excel(xls, sheet_name=nome_aba)
 
-                # Itera pelas linhas de "Epic" e gera os dados de "Hora/mês"
+                # Renomeia colunas (remove acentos e substitui espaços por underscores)
+                df.columns = [unidecode.unidecode(col).replace(" ", "_") for col in df.columns]
+
+                # Itera pelas linhas de "Epic" e gera os dados de "Hora_mes"
                 for index, row in df.iterrows():
-                    # Pega o valor do Epic (coluna A), Planned effort (coluna G), e Hora/mês (colunas H a S)
+                    # Pega o valor do Epic (coluna A), Planned effort (coluna G), e Hora_mes (colunas H a S)
                     epic = row.iloc[0]  # Valor da coluna A (Epic) usando iloc
                     planned_effort = row.iloc[6]  # Valor da coluna G (Planned effort) usando iloc
 
                     # Verifica se há dados nas colunas de H a S (12 meses)
-                    meses = ['Mês 1', 'Mês 2', 'Mês 3', 'Mês 4', 'Mês 5', 'Mês 6', 'Mês 7', 'Mês 8', 'Mês 9', 'Mês 10', 'Mês 11', 'Mês 12']
+                    meses = ['Mes_1', 'Mes_2', 'Mes_3', 'Mes_4', 'Mes_5', 'Mes_6', 'Mes_7', 'Mes_8', 'Mes_9', 'Mes_10', 'Mes_11', 'Mes_12']
                     horas_mes = row.iloc[7:19]  # Colunas H até S usando iloc
 
                     # Verifica se as células contêm dados válidos
                     if not horas_mes.isnull().all():
-                        # Cria uma linha para cada mês, com o respectivo valor de Hora/mês
+                        # Cria uma linha para cada mês, com o respectivo valor de Hora_mes
                         for i, hora in enumerate(horas_mes):
                             nova_linha = {
                                 'Epic': epic,
-                                'Planned effort': planned_effort,
-                                'Hora/mês': hora,
-                                'Mês': meses[i]
+                                'Planned_effort': planned_effort,
+                                'Hora_mes': hora,
+                                'Mes': meses[i]
                             }
                             lista_dfs.append(nova_linha)
 
@@ -272,15 +284,12 @@ def consolidar_horas_backlog_sharepoint(lista_arquivos_sharepoint, token):
 
         # Define o caminho para salvar a planilha consolidada
         url_diretorio_sharepoint = "https://weg365.sharepoint.com/sites/BR-TI-TIN/DEV_AlocacaoRecursos/Consolidado"
-        nome_arquivo_sharepoint = "consolidada-backlog-horas.xlsx"
+        nome_arquivo_sharepoint = "consolidada-backlog-horas.csv"
         
         # Salva o DataFrame em um objeto de memória (BytesIO), sem salvar localmente
-        # Salva o DataFrame em um objeto de memória (BytesIO), sem salvar localmente
         arquivo_memoria = BytesIO()
-        dataframe_consolidado.to_csv(arquivo_memoria, index=False)  # Mudança aqui para salvar como CSV
+        dataframe_consolidado.to_csv(arquivo_memoria, index=False)  # Salvando como CSV
         arquivo_memoria.seek(0)  # Move o ponteiro de volta ao início do arquivo
-
-        nome_arquivo_sharepoint = "consolidada-backlog-horas.csv"  # Mudança aqui para .csv
 
         try:
             # Faz o upload do arquivo diretamente da memória para o SharePoint
